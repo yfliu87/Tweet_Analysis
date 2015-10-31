897b03271872c7e90ccf701a84e51e60a98c3849
yfliu <yfliu87@gmail.com>
2015-10-31 16:24:37 +0800

    extract method analysis

--- pyspark_twitter.py
+++ pyspark_twitter.py
@@ -4,19 +4,22 @@ sys.setdefaultencoding('utf-8')
 
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setAppName("twitter")
-sc = SparkContext(conf = conf)
-textFile = sc.textFile("hdfs://quickstart.cloudera:8020/user/cloudera/yfliu/twitter_1000.json")
+def analysis():
+	conf = SparkConf().setAppName("twitter")
+	sc = SparkContext(conf = conf)
+	textFile = sc.textFile("hdfs://quickstart.cloudera:8020/user/cloudera/yfliu/twitter_1000.json")
 
-import json
-from operator import add
-filtered_rdd = textFile.map(lambda line: json.loads(line)).filter(lambda line: "text" in line)
-split_rdd = filtered_rdd.flatMap(lambda line: line["text"].split(" "))
-map_rdd = split_rdd.map(lambda item: (item, 1))
-output = map_rdd.reduceByKey(add).takeOrdered(10, key=lambda x: -x[1])
+	import json
+	from operator import add
+	filtered_rdd = textFile.map(lambda line: json.loads(line)).filter(lambda line: "text" in line)
+	split_rdd = filtered_rdd.flatMap(lambda line: line["text"].split(" "))
+	map_rdd = split_rdd.map(lambda item: (item, 1))
+	output = map_rdd.reduceByKey(add).takeOrdered(10, key=lambda x: -x[1])
 
-for (word, count) in output:
-	print ("%s: %i" %(word, count))
+	for (word, count) in output:
+		print ("%s: %i" %(word, count))
 
-sc.stop()
+		sc.stop()
 
+if __name__ == '__main__':
+	analysis()

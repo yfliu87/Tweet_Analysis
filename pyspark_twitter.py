47f8e477bac3441ace5f140a8f05c22cd11d0285
yfliu <yfliu87@gmail.com>
2015-10-31 14:45:34 +0800

    extract text from each tweet and count word

--- pyspark_twitter.py
+++ pyspark_twitter.py
@@ -1,6 +1,22 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
 from pyspark import SparkConf, SparkContext
 
 conf = SparkConf().setAppName("twitter")
 sc = SparkContext(conf = conf)
 textFile = sc.textFile("hdfs://quickstart.cloudera:8020/user/cloudera/yfliu/twitter_1000.json")
-print textFile.count()
+
+import json
+from operator import add
+filtered_rdd = textFile.map(lambda line: json.loads(line)).filter(lambda line: "text" in line)
+split_rdd = filtered_rdd.flatMap(lambda line: line["text"].split(" "))
+map_rdd = split_rdd.map(lambda item: (item, 1))
+output = map_rdd.reduceByKey(add).collect()
+
+for (word, count) in output:
+	print ("%s: %i" %(word, count))
+
+sc.stop()
+

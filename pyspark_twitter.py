bf851c855c8c0602af9858917b11701d4d3cc0e6
yfliu <yfliu87@gmail.com>
2015-10-31 22:10:16 +0800

    load json data in extracted readline method

--- pyspark_twitter.py
+++ pyspark_twitter.py
@@ -3,16 +3,23 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 from pyspark import SparkConf, SparkContext
+import json
+
 separator = [",",":","@"," ","!","#","$","%","*","...","(",")","~",""]
+
+def readline(line):
+	try:
+		return json.loads(line)
+	except:
+		return json.dumps([])
 
 def analysis():
 	conf = SparkConf().setAppName("twitter")
 	sc = SparkContext(conf = conf)
-	textFile = sc.textFile("hdfs://quickstart.cloudera:8020/user/cloudera/yfliu/twitter_1000.json")
+	textFile = sc.textFile("hdfs://quickstart.cloudera:8020/user/cloudera/yfliu/twitter_10000.json")
 
-	import json
 	from operator import add
-	text_rdd = textFile.map(lambda line: json.loads(line)).filter(lambda line: "text" in line)
+	text_rdd = textFile.map(lambda line: readline(line)).filter(lambda line: "text" in line)
 	filtered_rdd = text_rdd.filter(lambda line: line["lang"] == "en")
 	split_rdd = filtered_rdd.flatMap(lambda line: line["text"].split(" "))
 	no_separator_rdd = split_rdd.filter(lambda item: item not in separator)
